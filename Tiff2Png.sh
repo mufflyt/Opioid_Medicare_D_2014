e154306618efec7c135a2a40ca5685a79912f602
Diego F Coêlho <diegofcoelho@gmail.com>
2021-08-15 20:23:32 -0300

    Create Tiff2Png.sh
    A Imagemagick script to convert Tiff to Png

---
+++ Tiff2Png.sh
@@ -0,0 +1,8 @@
+
+convert FPMRS_OpioidRidgePlot.tiff FPMRS_OpioidRidgePlot.png
+
+convert OpioidHeatMap.tiff OpioidHeatMap.png
+
+convert DentistOpioidMeanMedianPlot.tiff DentistOpioidMeanMedianPlot.png
+
+convert OpioidBoxPlot.tiff OpioidBoxPlot.png
